3d154253e7eec20da404bce85fb7c049f729eed0
AmirAnckonina <50033620+AmirAnckonina@users.noreply.github.com>
2022-11-27 00:53:21 +0200

    Updates

--- A23 Ex01 AmirAnckonina 208423491 IdanZimilis 316133222/FBServiceLogic/DTOs/PostDTO.cs
+++ A23 Ex01 AmirAnckonina 208423491 IdanZimilis 316133222/FBServiceLogic/DTOs/PostDTO.cs
@@ -17,12 +17,12 @@ namespace FBServiceLogic.DTOs
 
         }
 
-        public PostDTO(string i_Message, string i_Caption, DateTime? i_DateTime)
+        /*public PostDTO(string i_Message, string i_Caption, DateTime? i_DateTime)
         {
             Message = i_Message;
             Caption = i_Message;
             CreatedTime = i_DateTime;
-        }
+        }*/
 
         
         
