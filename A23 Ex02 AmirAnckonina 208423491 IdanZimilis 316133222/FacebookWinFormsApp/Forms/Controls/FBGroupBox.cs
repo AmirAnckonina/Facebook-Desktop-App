228a0ea47c32c4f7517a15dbfe72e5a04d6d875f
AmirAnckonina <50033620+AmirAnckonina@users.noreply.github.com>
2023-02-06 19:09:32 +0200

    Ex2Start

---
+++ A23 Ex02 AmirAnckonina 208423491 IdanZimilis 316133222/FacebookWinFormsApp/Forms/Controls/FBGroupBox.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FacebookWinFormsApp.Forms.Controls
+{
+    public partial class FbGroupBox : UserControl
+    {
+        public FbGroupBox()
+        {
+            InitializeComponent();
+        }
+
+        public void SetGroupNameLabel(string i_AlbumName)
+        {
+           groupNameLabel.Text = i_AlbumName;
+        }
+
+        public void SetGroupPictureInPictureBox(string i_AlbumPictureURL)
+        {
+            groupPictureBox.LoadAsync(i_AlbumPictureURL);
+        }
+
+        public void SetGroupMembersCount(string i_MembersCount)
+        {
+            groupMembersLabel.Text = i_MembersCount;
+        }
+
+        public void SetGroupPrivacy(string i_GroupPrivacy)
+        {
+            privacyLabel.Text = i_GroupPrivacy;
+        }
+    }
+}
